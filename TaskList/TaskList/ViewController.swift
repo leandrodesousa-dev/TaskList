fd92acab81a5183e88dca3303f7731f7e4f3b124
Leandro <leandrooodesousa@gmail.com>
2019-09-17 07:36:05 -0400

    Created project xcode

---
+++ TaskList/TaskList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskList
+//
+//  Created by Leandro de Sousa Silva on 17/09/19.
+//  Copyright © 2019 AcademyMistic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
